2f5c9933aaf89757add2a5267123d367843011b4
jones <claudiojccoimbra@gmail.com>
2025-02-12 18:07:41 -0300

    mod

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,16 +3,24 @@ import { HeaderComponent } from './header/header.component';
 
 import { UserComponent } from './user/user.component';
 import { DUMMY_USERS } from './dummy-users';
-
+import { TasksComponent } from './tasks/tasks.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, UserComponent], 
+  standalone:true,
+  imports: [HeaderComponent, UserComponent, TasksComponent], 
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 
 export class AppComponent {
-  title = 'Angular19-Project';
   users = DUMMY_USERS;
+  selectedUserId?: string;
+
+  get selectedUser() {
+    return this.users.find((user) => user.id === this.selectedUserId);
+  }
+  onSelectUser(id: string) {
+    this.selectedUserId = id;
+  }
 }
