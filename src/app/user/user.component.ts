d1268415dc25380a9b6ac6833f4a136b91abd1bc
jones <claudiojccoimbra@gmail.com>
2025-02-12 18:05:57 -0300

    modified user component

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -3,6 +3,18 @@ import { Component, Input, Output, EventEmitter, output } from '@angular/core'; 
 //import { DUMMY_USERS } from '../dummy-users';
 //const randonIndex = Math.floor(Math.random() * DUMMY_USERS.length)
 
+//type User = {
+//  id: string;
+//  avatar: string;
+//  name: string;
+//}
+
+interface User {
+  id: string;
+  avatar: string;
+  name: string;
+}
+
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -22,11 +34,7 @@ export class UserComponent {
   //@Input({ required:true }) avatar!: string;
   //@Input({ required: true }) name!: string;
 
-  @Input({ required: true }) user!: {
-    id: string;
-    avatar: string;
-    name: string;
-  };
+  @Input({ required: true }) user!: User;
 
   @Output() select = new EventEmitter<string>();
   //select = output<string>();
