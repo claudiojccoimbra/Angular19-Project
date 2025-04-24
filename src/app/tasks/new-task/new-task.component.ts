26dadc3025f71aa07e97568ca6233f602ebc74ac
jones <claudiojccoimbra@gmail.com>
2025-04-24 20:08:44 -0300

    add inject core

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,6 +1,7 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { NewTaskData } from '../task/task.model';
+import { TasksService } from '../tasks.service';
 
 @Component({
   selector: 'app-new-task',
@@ -10,22 +11,31 @@ import { NewTaskData } from '../task/task.model';
   styleUrl: './new-task.component.css'
 })
 export class NewTaskComponent {
+  @Input({ required: true }) userId! : string;
   @Output() cancel = new EventEmitter<void>()
-  @Output() add = new EventEmitter<NewTaskData>();
-
+  //@Output() add = new EventEmitter<NewTaskData>();
   enteredTitle = '';
   enteredSummary = '';
   enteredDate = '';
+
+  private tasksService = inject(TasksService);
+
 
   onCancel() {
     this.cancel.emit();
   }
 
   onSubmit() {
-    this.add.emit({
+
+    this.tasksService.addTask({
       title: this.enteredTitle,
       summary: this.enteredSummary,
       date: this.enteredDate
-    });
+    }, '')
+    //this.add.emit({
+    //  title: this.enteredTitle,
+    //  summary: this.enteredSummary,
+    //  date: this.enteredDate
+    //});
   }
 }
